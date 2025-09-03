fba30f09968afa5e52cae488f6aadf7b5af8786d
HayatoG <guilherme.ryders@gmail.com>
2025-09-02 21:46:07 -0300

    feat: adicionando comentário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -396,7 +396,7 @@ export default function Home() {
         link.download = fileName;
         link.target = '_blank';
         link.rel = 'noopener noreferrer';
-        link.style.display = 'none';
+        link.style.display = 'none'; // Teste
 
         document.body.appendChild(link);
         link.click();
